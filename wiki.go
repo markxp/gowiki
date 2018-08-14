f2b8b31b1fdf4ddec3d9449b1acc16ef05bdb289
markxp <markxp2002@gmail.com>
2018-08-14 16:14:26 +0800

    Update wiki.go

--- wiki.go
+++ wiki.go
@@ -5,8 +5,11 @@ import (
 )
 
 func main() {
+	// register to http default serve mux
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
+	
+	// listen to :6060, use default serve mux
 	http.ListenAndServe(":6060", nil)
 }
